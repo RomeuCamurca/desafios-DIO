294e00a7fb25c84aaf400529cbfd7e2eb552203f
romeucamurca <romeucamurcarabelo@gmail.com>
2021-02-04 23:32:27 -0300

    update solution Quanta Mandioca

--- Desafios/JavaScript/1. Introdução a Programação/1.3. Quanta Mandioca/solucao.js
+++ Desafios/JavaScript/1. Introdução a Programação/1.3. Quanta Mandioca/solucao.js
@@ -4,5 +4,5 @@ let bernardo = 600 * parseInt(gets());
 let marina = 1000 * parseInt(gets());
 let iara = 150 * parseInt(gets());
 let marlene = 225;
-let total = chico + bento + bernardo + marina + iara + marlene; // Digite aqui o calculo total
+let total = chico + bento + bernardo + marina + iara + marlene; 
 console.log(total);
